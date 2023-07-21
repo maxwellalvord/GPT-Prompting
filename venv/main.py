17e41a94467ce261247ef69b062656df37171b02
Maxwell Alvord <maxwellalvord@gmail.com>
2023-07-21 13:30:06 -0700

    WIP main.py: add proper context for medical assistant chat AI

--- venv/main.py
+++ venv/main.py
@@ -6,14 +6,14 @@ panels = []
 
 openai.api_key  = ('OPENAI_API_KEY')
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
+# def get_completion(prompt, model="gpt-3.5-turbo"):
+#     messages = [{"role": "user", "content": prompt}]
+#     response = openai.ChatCompletion.create(
+#         model=model,
+#         messages=messages,
+#         temperature=0, # this is the degree of randomness of the model's output
+#     )
+#     return response.choices[0].message["content"]
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
     response = openai.ChatCompletion.create(
         model=model,
@@ -36,33 +36,11 @@ def collect_messages(_):
     return pn.Column(*panels)
 
 context = [ {'role':'system', 'content':"""
-You are OrderBot, an automated service to collect orders for a pizza restaurant. \
-You first greet the customer, then collects the order, \
-and then asks if it's a pickup or delivery. \
-You wait to collect the entire order, then summarize it and check for a final \
-time if the customer wants to add anything else. \
-If it's a delivery, you ask for an address. \
-Finally you collect the payment.\
-Make sure to clarify all options, extras and sizes to uniquely \
-identify the item from the menu.\
-You respond in a short, very conversational friendly style. \
-The menu includes \
-pepperoni pizza  12.95, 10.00, 7.00 \
-cheese pizza   10.95, 9.25, 6.50 \
-eggplant pizza   11.95, 9.75, 6.75 \
-fries 4.50, 3.50 \
-greek salad 7.25 \
-Toppings: \
-extra cheese 2.00, \
-mushrooms 1.50 \
-sausage 3.00 \
-canadian bacon 3.50 \
-AI sauce 1.50 \
-peppers 1.00 \
-Drinks: \
-coke 3.00, 2.00, 1.00 \
-sprite 3.00, 2.00, 1.00 \
-bottled water 5.00 \
+You are a Medical students AI assistant and you are to be verbose and cite academic articles used to determine the answer. \
+If unsure of the answer you will ask the user clarifying questions. \
+You first greet the user, then ask when the question is and what medical field it relates to. \
+Allow time to verify your answer with cited academic written pieces. \
+Finally ask the user if you answered their question and if they have any more. \
 """} ] 
 
 
