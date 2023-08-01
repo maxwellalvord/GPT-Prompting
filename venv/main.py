49c8b1db7c66dd0961bba9844313333e222d57e0
Maxwell Alvord <maxwellalvord@gmail.com>
2023-08-01 16:10:30 -0700

    Update main.py: delete unneeded helper function and customize summation prompt.

--- venv/main.py
+++ venv/main.py
@@ -6,14 +6,6 @@ panels = []
 
 openai.api_key  = ('OPENAI_API_KEY')
 
-# def get_completion(prompt, model="gpt-3.5-turbo"):
-#     messages = [{"role": "user", "content": prompt}]
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=messages,
-#         temperature=0, # this is the degree of randomness of the model's output
-#     )
-#     return response.choices[0].message["content"]
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
     response = openai.ChatCompletion.create(
         model=model,
@@ -65,22 +57,12 @@ dashboard
 # {'role':'user', 'content':'tell me a joke'},   
 # {'role':'assistant', 'content':'Why did the chicken cross the road'},   
 # {'role':'user', 'content':'I don\'t know'}  ]
-background1 = """
-You are a Medical students AI assistant and you are to be verbose and cite academic articles used to determine the answer. 
-"""
-background2 ="""
-You are a AI assistant to radio host and will be asked to find information of songs their artists, when asked to find a song based on user prompt you will give your answer by first telling the Song name and Artist and the album then following that you will answer the users remaining question.
-"""
-
-
 
 
 messages =  context.copy()
 messages.append(
-{'role':'system', 'content':'create a json summary of the previous food order. Itemize the price for each item\
- The fields should be 1) pizza, include size 2) list of toppings 3) list of drinks, include size   4) list of sides include size  5)total price '},    
+{'role':'system', 'content':'create a json summary of the previous answer with one sentence.'},    
 )
- #The fields should be 1) pizza, price 2) list of toppings 3) list of drinks, include size include price  4) list of sides include size include price, 5)total price '},    
 
 response = get_completion_from_messages(messages, temperature=0)
 print(response)
