cb6c348009052364d29f31ff1004cdf0c295649a
Maxwell Alvord <maxwellalvord@gmail.com>
2023-08-02 15:48:55 -0700

    Update main.py: re add helper function and get interface working in juypter notebook.

--- venv/main.py
+++ venv/main.py
@@ -1,10 +1,15 @@
 import openai
-import panel as pn  
-pn.extension()
-
-panels = []
 
 openai.api_key  = ('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
 
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
     response = openai.ChatCompletion.create(
@@ -30,6 +35,10 @@ def collect_messages(_):
  
     return pn.Column(*panels)
 
+import panel as pn 
+pn.extension()
+
+panels = []
 
 context = [ {'role':'system', 'content':"""
 You are a Medical students AI assistant and you are to be verbose and cite academic articles used to determine the answer. \
@@ -51,11 +60,3 @@ dashboard = pn.Column(
 )
 
 dashboard
-
-messages =  context.copy()
-messages.append(
-{'role':'system', 'content':'create a json summary of the previous answer with one sentence.'},    
-)
-
-response = get_completion_from_messages(messages, temperature=0)
-print(response)
